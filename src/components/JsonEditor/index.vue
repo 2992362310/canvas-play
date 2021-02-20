8fc08cafb59f5f537d6ca7b7dab97188e4b48719
GDO <29923632310@qq.com>
2021-02-20 16:41:46 +0800

    添加vue-json-editor

---
+++ src/components/JsonEditor/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>vue-json-editor</p>
+        <vue-json-editor v-model="json" :show-btns="true" :expandedOnStart="true" @json-change="onJsonChange"></vue-json-editor>
+    </div>
+</template>
+
+<script>
+    import vueJsonEditor from 'vue-json-editor'
+    import mapconfig from './mapconfig.json'
+
+    export default {
+        data() {
+            return {
+                json: mapconfig
+            }
+        },
+
+        components: {
+            vueJsonEditor
+        },
+
+        methods: {
+            onJsonChange(value) {
+                console.log('value:', value)
+            }
+        }
+    }
+</script>
